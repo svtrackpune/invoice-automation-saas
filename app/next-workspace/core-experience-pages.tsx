23b25b6a8bb361181918729e691ed4c9b8a34f9c
svtrackpune <svtrackpune@gmail.com>
2026-08-23 14:14:48 +0530

    Add core experience route helpers

---
+++ app/next-workspace/core-experience-pages.tsx
@@ -0,0 +1,3 @@
+'use client';
+import { Customer360, MoneyIn, MoneyOut, Payments, Banking, Products, Reports, TaxCenter, BusinessHealth } from './core-experience';
+export const corePages = { Customer360, MoneyIn, MoneyOut, Payments, Banking, Products, Reports, TaxCenter, BusinessHealth };
