896095b72f5e471e885804ebc416e2905e8d35c6
svtrackpune <svtrackpune@gmail.com>
2026-08-24 12:20:54 +0530

    refactor: compose document settings workspace

---
+++ app/next-workspace/brand/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import UnifiedDocumentSettings from './UnifiedDocumentSettings';
+import DocumentBankAccountsPanel from './DocumentBankAccountsPanel';
+
+export default function BrandPage(){
+  return <>
+    <UnifiedDocumentSettings />
+    <DocumentBankAccountsPanel />
+  </>;
+}
