dd6741c6a9fe0c807ec7e7bab21cf81c90109550
svtrackpune <svtrackpune@gmail.com>
2026-08-24 12:21:55 +0530

    style: wrap document settings in polished shell

--- app/next-workspace/brand/page.tsx
+++ app/next-workspace/brand/page.tsx
@@ -3,8 +3,8 @@ import UnifiedDocumentSettings from './UnifiedDocumentSettings';
 import DocumentBankAccountsPanel from './DocumentBankAccountsPanel';
 
 export default function BrandPage(){
-  return <>
+  return <main className="document-settings-shell"><div className="document-settings-inner">
     <UnifiedDocumentSettings />
     <DocumentBankAccountsPanel />
-  </>;
+  </div></main>;
 }
