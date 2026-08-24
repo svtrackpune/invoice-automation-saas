a430473f20a00da5e60e4d7cfb041518e9c6e052
svtrackpune <svtrackpune@gmail.com>
2026-08-24 19:15:46 +0530

    feat: load modern document template styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./document-templates.css";
 import ModalPersistenceGuard from "./ModalPersistenceGuard";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
