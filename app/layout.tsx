37e39b68052f92e0487ac37886618c6a3447efa6
svtrackpune <svtrackpune@gmail.com>
2026-08-25 17:03:33 +0530

    feat: apply final document branding styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./document-templates.css";
+import "./document-branding-final.css";
 import ModalPersistenceGuard from "./ModalPersistenceGuard";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
